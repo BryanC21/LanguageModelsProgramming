5442db627ce9fa2ce904f59f84a3a2edd115ecb6
Bryan Caldera <bryanicaldera@gmail.com>
2023-02-09 19:53:24 -0800

    sentiment analysis

---
+++ Part1.py
@@ -0,0 +1,14 @@
+from textblob import TextBlob
+
+def sentiment_analysis(text):
+    analysis = TextBlob(text)
+    if analysis.sentiment.polarity > 0:
+        return "Positive"
+    elif analysis.sentiment.polarity == 0:
+        return "Neutral"
+    else:
+        return "Negative"
+
+text = input("Enter a text string: ")
+sentiment = sentiment_analysis(text)
+print("Sentiment: ", sentiment)
